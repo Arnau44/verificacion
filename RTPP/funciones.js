01441be2b498688b30b40cd57af846f04d434dd6
Arnau44 <52758017+Arnau44@users.noreply.github.com>
2019-08-01 09:01:28 +0200

    Add files via upload

--- RTPP/funciones.js
+++ RTPP/funciones.js
@@ -1,1 +1,18 @@
-fu
+function disable() {
+    confirm ("Una vez guardado, no podras modificarlo en un dia")
+}
+function habilitar() {
+    document.getElementById("horaDeEntrada").disabled = false;
+
+    document.getElementById("horaDeMargen").disabled = false;
+}
+
+function disable(){
+    var doc;
+    var doc2;
+    var result = confirm("¿Estas seguro?");
+    if (result == true){
+        doc = document.getElementById("horaDeEntrada").disabled = true;
+        document.getElementById("horaDeMargen").disabled = true;
+    }
+}
